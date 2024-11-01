43b4d64c4f3584291ecc9cc6d2671203add2641c
ForsakenAginor <juriys2022@mail.ru>
2024-11-01 20:33:03 +0400

    add base menu options

---
+++ Assets/Scripts/General/SceneChangeButton.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class SceneChangeButton : MonoBehaviour
+{
+    [SerializeField] private Scenes _scene;
+    private Button _button;
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(OnButtonClick);
+    }
+
+    private void OnDestroy()
+    {
+        _button.onClick.RemoveListener(OnButtonClick);        
+    }
+
+    private void OnButtonClick()
+    {
+        SceneManager.LoadScene(_scene.ToString());
+    }
+}
