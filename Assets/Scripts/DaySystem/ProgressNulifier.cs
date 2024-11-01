7861f299fb260f50bf3939c9a98702df43db10bd
ForsakenAginor <juriys2022@mail.ru>
2024-11-02 00:00:16 +0400

    add daySystem

---
+++ Assets/Scripts/DaySystem/ProgressNulifier.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ProgressNulifier : MonoBehaviour
+{
+    private void Start()
+    {
+        const string DayDataKey = nameof(DayDataKey);
+
+        if (PlayerPrefs.HasKey(DayDataKey))
+            PlayerPrefs.DeleteKey(DayDataKey);
+    }
+}
