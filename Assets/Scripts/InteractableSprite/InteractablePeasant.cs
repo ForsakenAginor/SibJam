9fb72c5ffc023743974589ec920bc7e9358d3448
ForsakenAginor <juriys2022@mail.ru>
2024-11-02 20:28:41 +0400

    basic peasant animations

--- Assets/Scripts/InteractableSprite/InteractablePeasant.cs
+++ Assets/Scripts/InteractableSprite/InteractablePeasant.cs
@@ -1,16 +1,28 @@
 ﻿using UnityEngine.EventSystems;
 using UnityEngine;
+using System.Collections;
+using DG.Tweening;
 
 [RequireComponent(typeof(Collider2D))]
 public class InteractablePeasant : MonoBehaviour, IPointerDownHandler
 {
+    [SerializeField] private float _animationDuration;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Transform _targetPoint;
+
     private UIElement _windowThatWillBeOpened;
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("down");
-        _windowThatWillBeOpened?.Enable();
+        if (_windowThatWillBeOpened == null)
+            return;
+
+        _windowThatWillBeOpened.Enable();
         _windowThatWillBeOpened = null;
+
+        _spriteRenderer.flipX = true;
+        Vector3 target = new Vector3(_targetPoint.position.x, transform.position.y, transform.position.z);
+        transform.DOMove(target, _animationDuration).SetEase(Ease.Linear);
     }
 
     public void Init(UIElement uIElement)
