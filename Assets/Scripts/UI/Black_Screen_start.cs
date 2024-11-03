e750f6c99c13f4646e0ef4874156f8fae72aced1
Waterfall17 <daniilwaterfall@mail.ru>
2024-11-03 16:51:22 +0400

    Win/Lose screen game scene, new black screen animation
    after merge it may be broken

--- Assets/Scripts/UI/Black_Screen_start.cs
+++ Assets/Scripts/UI/Black_Screen_start.cs
@@ -9,6 +9,7 @@ public class Black_Screen_start : MonoBehaviour
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+        _animator.SetBool("End", true);
     }
 
     public void Anim()
