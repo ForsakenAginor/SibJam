0c6747de92974168fe363e831c87622415d106c4
ForsakenAginor <juriys2022@mail.ru>
2024-11-04 00:27:49 +0400

    final hero animtion

--- Assets/Scripts/InteractableSprite/DeskSpriteChanger.cs
+++ Assets/Scripts/InteractableSprite/DeskSpriteChanger.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -14,13 +16,17 @@ public class DeskSpriteChanger : MonoBehaviour
 
     [SerializeField] private InteractableSprite _interactableSprite;
 
+    [SerializeField] private UIElement _questionMark;
+
     private SpriteRenderer _renderer;
     private Desk _desk;
 
     private void Awake()
     {
         _renderer = GetComponent<SpriteRenderer>();
-        _interactableSprite.Pressed += OnContainChanged;
+
+        if (_interactableSprite != null)
+            _interactableSprite.Pressed += OnContainChanged;
     }
 
     public void Init(Desk table)
@@ -31,6 +37,11 @@ public class DeskSpriteChanger : MonoBehaviour
         _desk.QuestRemoved += OnContainChanged;
     }
 
+    public void Init(int quests)
+    {
+        OnContainChanged(quests, true);
+    }
+
     private void OnContainChanged(Quest _)
     {
         OnContainChanged();
@@ -38,12 +49,12 @@ public class DeskSpriteChanger : MonoBehaviour
 
     private void OnContainChanged()
     {
-        if(_desk.Quests.Count() == 0)
+        if (_desk.Quests.Count() == 0)
         {
             _renderer.sprite = _emptyDesk;
             _mark.Disable();
         }
-        else if(_desk.Quests.Count() == 1)
+        else if (_desk.Quests.Count() == 1)
         {
             _renderer.sprite = _oneQuestDesk;
             _mark.Enable();
@@ -58,5 +69,72 @@ public class DeskSpriteChanger : MonoBehaviour
             _renderer.sprite = _fullDesk;
             _mark.Enable();
         }
+    }
+
+    private void OnContainChanged(int quests, bool isFirstCall)
+    {
+        if (quests == 0 && isFirstCall == true)
+        {
+            _renderer.sprite = _emptyDesk;
+            _mark.Disable();
+        }
+        else if(quests == 0 && isFirstCall == false)
+        {
+            _renderer.sprite = _emptyDesk;
+            _mark.Disable();
+            _questionMark.Enable();
+        }
+        else if (quests == 1)
+        {
+            _renderer.sprite = _oneQuestDesk;
+            _mark.Enable();
+
+            if (isFirstCall)
+            {
+                StartCoroutine(ChangeDesk(quests - 1));
+            }
+            else
+            {
+                _mark.Disable();
+                _questionMark.Enable();
+            }
+        }
+        else if (quests == 3)
+        {
+            _renderer.sprite = _threeQuestDesk;
+            _mark.Enable();
+
+            if (isFirstCall)
+            {
+                StartCoroutine(ChangeDesk(quests - 1));
+            }
+            else
+            {
+                _mark.Disable();
+                _questionMark.Enable();
+            }
+        }
+        else if (quests > 3)
+        {
+            _renderer.sprite = _fullDesk;
+            _mark.Enable();
+
+            if (isFirstCall)
+            {
+                StartCoroutine(ChangeDesk(quests - 1));
+            }
+            else
+            {
+                _mark.Disable();
+                _questionMark.Enable();
+            }
+        }
+    }
+
+    private IEnumerator ChangeDesk(int quests)
+    {
+        WaitForSeconds delay = new WaitForSeconds(5f);
+        yield return delay;
+        OnContainChanged(quests, false);
     }
 }
