9acda8008e89c096829c4746afa63010f4844b60
ForsakenAginor <juriys2022@mail.ru>
2024-11-11 15:50:47 +0400

    add languages sprites

--- Assets/Scripts/EntryPoint/BootsTrap.cs
+++ Assets/Scripts/EntryPoint/BootsTrap.cs
@@ -1,26 +1,21 @@
 using Assets.Scripts.General;
-using Assets.Scripts.UnityAnalytics;
 using Localization;
+using Unity.Services.Analytics;
 using Unity.Services.Core;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class BootsTrap : MonoBehaviour
 {
-    [SerializeField] private AnalyticGameObject _analyticsService;
-
     private async void Start()
     {
         LocalizationInitializer localization = new LocalizationInitializer();
 
         localization.ApplyLocalization("en");
-        await _analyticsService.Init();
-        //InitializeServices();
-        SceneManager.LoadScene(Scenes.MainMenu.ToString());
-    }
-
-    private async void InitializeServices()
-    {
         await UnityServices.InitializeAsync();
+        Debug.Log("Analytic initialized");
+        AnalyticsService.Instance.StartDataCollection();
+        Debug.Log("Analytic data collection start");
+        SceneManager.LoadScene(Scenes.MainMenu.ToString());
     }
 }
