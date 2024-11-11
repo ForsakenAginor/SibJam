25ea05e80e9bedb4be784ae8e0b5c1c5e16da25e
ForsakenAginor <juriys2022@mail.ru>
2024-11-11 14:40:36 +0400

    unity analytics implementation

--- Assets/Scripts/EntryPoint/BootsTrap.cs
+++ Assets/Scripts/EntryPoint/BootsTrap.cs
@@ -1,14 +1,26 @@
 using Assets.Scripts.General;
+using Assets.Scripts.UnityAnalytics;
 using Localization;
+using Unity.Services.Core;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class BootsTrap : MonoBehaviour
 {
-    private void Start()
+    [SerializeField] private AnalyticGameObject _analyticsService;
+
+    private async void Start()
     {
         LocalizationInitializer localization = new LocalizationInitializer();
+
         localization.ApplyLocalization("en");
+        await _analyticsService.Init();
+        //InitializeServices();
         SceneManager.LoadScene(Scenes.MainMenu.ToString());
+    }
+
+    private async void InitializeServices()
+    {
+        await UnityServices.InitializeAsync();
     }
 }
---
+++ Assets/Scripts/UnityAnalytics/AnalyticGameObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Unity.Services.Analytics;
+using Unity.Services.Core;
+using UnityEngine;
+
+namespace Assets.Scripts.UnityAnalytics
+{
+    public class AnalyticGameObject : MonoBehaviour
+    {
+        private void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public async Task Init()
+        {
+            await UnityServices.InitializeAsync();
+            Debug.Log("Analytic initialized");
+            AnalyticsService.Instance.StartDataCollection();
+            Debug.Log("Analytic data collection start");
+        }
+    }
+}
